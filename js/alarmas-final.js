d31ebfd077852b9d811b88f3c6f1f954f351343d
slakat <slakat@gmail.com>
2018-07-11 17:54:40 -0400

    nuevos datos actualizados

--- js/alarmas-final.js
+++ js/alarmas-final.js
@@ -2,75 +2,75 @@
 // Data is joined to map using value of 'hc-key' property by default.
 // See API docs for 'joinBy' for more info on linking data and map.
 var data_expansion = [
-['cl-an', 7], //Antofagasta
-['cl-2730', 23], //Araucanía
-['cl-2740', ], //Arica y Parinacota
-['cl-at', 5], //Atacama
-['cl-ai', 5], //Aysén
-['cl-bi', 14], //Bíobío
-['cl-co', 7], //Coquimbo
-['cl-ar', 5], //De Los Ríos
-['cl-ll', 22], //Los Lagos
-['cl-ma', 3], //Magallanes
-['cl-ml', 1], //Maule
-['cl-rm', 3], //Metropolitana
-['cl-li', 5], //O'Higgins
-['cl-ta', 4], //Tarapacá
-['cl-vs', 7], //Valparaíso
+['cl-an',7],//Antofagasta
+['cl-2730',27],//Araucanía
+['cl-2740',1],//Arica y Parinacota
+['cl-at',6],//Atacama
+['cl-ai',5],//Aysén
+['cl-bi',24],//Bíobío
+['cl-co',8],//Coquimbo
+['cl-ar',10],//De Los Ríos
+['cl-ll',22],//Los Lagos
+['cl-ma',4],//Magallanes
+['cl-ml',1],//Maule
+['cl-rm',3],//Metropolitana
+['cl-li',5],//O'Higgins
+['cl-ta',4],//Tarapacá
+['cl-vs',8],//Valparaíso
 ];
 
 var data_colapso = [
-['cl-an', 3], //Antofagasta
-['cl-2730', 29], //Araucanía
-['cl-2740', ], //Arica y Parinacota
-['cl-at', 7], //Atacama
-['cl-ai', 2], //Aysén
-['cl-bi', 20], //Bíobío
-['cl-co', 8], //Coquimbo
-['cl-ar', 4], //De Los Ríos
-['cl-ll', 20], //Los Lagos
-['cl-ma', 3], //Magallanes
-['cl-ml', 4], //Maule
-['cl-rm', 2], //Metropolitana
-['cl-li', 8], //O'Higgins
-['cl-ta', 5], //Tarapacá
-['cl-vs', 9] //Valparaíso
+['cl-an',3],//Antofagasta
+['cl-2730',33],//Araucanía
+['cl-2740',2],//Arica y Parinacota
+['cl-at',7],//Atacama
+['cl-ai',2],//Aysén
+['cl-bi',26],//Bíobío
+['cl-co',11],//Coquimbo
+['cl-ar',7],//De Los Ríos
+['cl-ll',22],//Los Lagos
+['cl-ma',3],//Magallanes
+['cl-ml',4],//Maule
+['cl-rm',2],//Metropolitana
+['cl-li',8],//O'Higgins
+['cl-ta',5],//Tarapacá
+['cl-vs',12],//Valparaíso
 ];
 
 var data_sanitario = [
-['cl-an', 7], //Antofagasta
-['cl-2730', 42], //Araucanía
-['cl-2740', ], //Arica y Parinacota
-['cl-at', 3], //Atacama
-['cl-ai', 4], //Aysén
-['cl-bi', 51], //Bíobío
-['cl-co', 11], //Coquimbo
-['cl-ar', 9], //De Los Ríos
-['cl-ll', 15], //Los Lagos
-['cl-ma', 7], //Magallanes
-['cl-ml', 2], //Maule
-['cl-rm', 5], //Metropolitana
-['cl-li', 9], //O'Higgins
-['cl-ta', 2], //Tarapacá
-['cl-vs', 15], //Valparaíso
+['cl-an',5],//Antofagasta
+['cl-2730',42],//Araucanía
+['cl-2740',],//Arica y Parinacota
+['cl-at',3],//Atacama
+['cl-ai',10],//Aysén
+['cl-bi',51],//Bíobío
+['cl-co',11],//Coquimbo
+['cl-ar',11],//De Los Ríos
+['cl-ll',14],//Los Lagos
+['cl-ma',],//Magallanes
+['cl-ml',3],//Maule
+['cl-rm',5],//Metropolitana
+['cl-li',9],//O'Higgins
+['cl-ta',3],//Tarapacá
+['cl-vs',14],//Valparaíso
 ];
 
 var data_ambiente = [
-['cl-an', ], //Antofagasta
-['cl-2730', 8], //Araucanía
-['cl-2740', ], //Arica y Parinacota
-['cl-at', 3], //Atacama
-['cl-ai', 1], //Aysén
-['cl-bi', 6], //Bíobío
-['cl-co', 11], //Coquimbo
-['cl-ar', 1], //De Los Ríos
-['cl-ll', 7], //Los Lagos
-['cl-ma', 2], //Magallanes
-['cl-ml', 2], //Maule
-['cl-rm', 1], //Metropolitana
-['cl-li', 4], //O'Higgins
-['cl-ta', 1], //Tarapacá
-['cl-vs', 3], //Valparaíso
+['cl-an',7],//Antofagasta
+['cl-2730',8],//Araucanía
+['cl-2740',],//Arica y Parinacota
+['cl-at',3],//Atacama
+['cl-ai',1],//Aysén
+['cl-bi',9],//Bíobío
+['cl-co',12],//Coquimbo
+['cl-ar',3],//De Los Ríos
+['cl-ll',7],//Los Lagos
+['cl-ma',2],//Magallanes
+['cl-ml',2],//Maule
+['cl-rm',1],//Metropolitana
+['cl-li',4],//O'Higgins
+['cl-ta',2],//Tarapacá
+['cl-vs',4],//Valparaíso
 ];
 
 
